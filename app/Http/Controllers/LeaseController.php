ae9ec6800c38bda1fe2410d0c2651f3ee3487435
nimol <limnimol.dev@gmail.com>
2026-04-20 12:29:07 +0700

    creatate migrate,model,control done all and work

--- app/Http/Controllers/LeaseController.php
+++ app/Http/Controllers/LeaseController.php
@@ -15,51 +15,5 @@ class LeaseController extends Controller
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Lease $lease)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Lease $lease)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Lease $lease)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Lease $lease)
-    {
-        //
-    }
+   
 }
